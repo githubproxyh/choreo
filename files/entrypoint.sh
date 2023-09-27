2bad29e28ee32622d1a9112dba6fefdbd4f08787
githubproxyh <129563090+githubproxyh@users.noreply.github.com>
2023-09-27 16:51:24 +0800

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'d342d11e-d575-4583-b36e-524ab1f0afa4'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
